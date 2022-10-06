60153f47a4cd4f104395820207432368016cc501
Amorim <matheusnavesamorim@gmail.com>
2022-10-06 01:37:07 -0300

    conectando com deploy

--- src/components/services/MyWallet.js
+++ src/components/services/MyWallet.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://mywalletamorim.herokuapp.com';
 
 function createHeaders() {
     const auth = JSON.parse(localStorage.getItem('mywallet'));
